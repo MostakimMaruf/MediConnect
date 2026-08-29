dd3cc95755dbc440af962a76d551b532c7c5bf51
mahmudajhumur <designer.mahmuda.co@gmail.com>
2026-08-29 23:51:21 +0600

    Successfully Implemented Specialization Module

--- config/db-connect.php
+++ config/db-connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mediconnect";
+$dbname = "doctor_appointment_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, 3306);
